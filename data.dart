8db28cf51615fca444085ecea308f421913e03de
F1A2I3-12 <faisalshingiro10@gmail.com>
2024-03-01 20:50:24 +0200

    first commit

---
+++ data.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Integers: Represent whole numbers
+  int age = 25;
+
+  // Double: Represent decimal numbers
+  double weight = 65.5;
+
+  // String: Represents a sequence of characters
+  String name = "Faisal SHINGIRO";
+
+  // List: Represents an ordered collection of objects
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Map: Represents a collection of key-value pairs
+  Map<String, dynamic> person = {'name': 'Alice', 'age': 30, 'isStudent': true};
+
+  // Printing out the values
+  print('Age: $age');
+  print('Weight: $weight');
+  print('Name: $name');
+  print('Numbers: $numbers');
+  print('Person: $person');
+}
